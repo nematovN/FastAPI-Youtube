6d4fd14055552203c0688271083a5e48aa6129a3
nematovN <nemat8954@gmail.com>
2025-03-05 23:56:47 +0500

    Add requirements.txt

--- downloader.py
+++ downloader.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI, HTTPException
-import yt_dlp as youtube_dl  # youtube-dl o'rniga yt-dlp ishlatamiz
+import youtube_dl
 import os
 
 app = FastAPI()
 
+
 @app.get("/download/")
 async def download_video(url: str):
     try:
-        # yt-dlp sozlamalari
+        # youtube-dl sozlamalari
         ydl_opts = {
             'format': 'best',
             'outtmpl': 'downloads/%(title)s.%(ext)s',  # Saqlash joyi
         }
 
-        # yt-dlp ni ishga tushirish
+        # youtube-dl ni ishga tushirish
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
             title = info_dict.get('title', None)
@@ -21,3 +22,4 @@ async def download_video(url: str):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail="Video yuklashda xato yuz berdi.")
+
